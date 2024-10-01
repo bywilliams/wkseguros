f1c15d67933d99d550075e4e451e81a57b44192f
bywilliams <willian.seu@gmail.com>
2024-09-30 22:55:44 -0300

    Atualição de dashboard layout e designer

--- wkseguros/frontend/app.py
+++ wkseguros/frontend/app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
 from datetime import datetime
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
+def login():
+    return render_template('app/login.html')
+
+
+@app.route('/home')
 def index():
     current_year = datetime.now().year
     return render_template('site/home.html', current_year=current_year)
--- wkseguros/tests/test_app.py
+++ wkseguros/tests/test_app.py
@@ -1,7 +1,13 @@
+from datetime import datetime
 from http import HTTPStatus
 
 
-def test_home(client):
-    reponse = client.get('/')
+def test_index(client):
+    """Testa o endpoint / para garantir
+    que a página inicial é renderizada corretamente"""
+    response = client.get('/')
 
-    assert reponse.status_code == HTTPStatus.OK
+    assert response.status_code == HTTPStatus.OK
+    assert 'Bem-vindo à Página Inicial' in response.data.decode('utf-8')
+    current_year = datetime.now().year
+    assert str(current_year) in response.data.decode()
