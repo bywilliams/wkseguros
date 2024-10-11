817ac632775cb96046f7350fe93ade2c704e251c
bywilliams <willian.seu@gmail.com>
2024-10-10 21:50:37 -0300

    feat: configurando tabela access_level e adiciona campo created_at na tabela user

--- wkseguros/backend/models/user.py
+++ wkseguros/backend/models/user.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 from sqlalchemy import Column, Integer, String
 
 from wkseguros.config import Base
@@ -10,3 +11,4 @@ class User(Base):
     email = Column(String, unique=True, index=True, nullable=False)
     password = Column(String, nullable=False)
     access_level = Column(Integer, nullable=False)
+    created_at = Column(datetime, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
